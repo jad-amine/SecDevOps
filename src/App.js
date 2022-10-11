32289bc45f17ac7b9def81cba9348e51df7a8f1d
jad amine <jade1992amine@gmail.com>
2022-10-11 20:37:51 +0300

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <LandingPage />
     </div>
   );
 }
