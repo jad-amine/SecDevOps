b9a1e0a5266fa63700b69ae7332f1734394f5d0e
jad amine <jade1992amine@gmail.com>
2022-10-11 20:37:34 +0300

    Added Landing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,18 @@
 import React from "react";
+const logo = require("../assets/LogoBlack.png");
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav">
+      <img src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>fb</li>
+        <li>inst</li>
+        <li>LinkedIn</li>
+      </ul>
+    </div>
+  );
 }
