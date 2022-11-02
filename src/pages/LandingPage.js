eb34b657e6891afd3b469a823c408ad18d285f8e
jad amine <jade1992amine@gmail.com>
2022-11-02 21:47:48 +0200

    Added Landing Page Text

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Home from "../components/LandingPage/Home";
 import Navbar from "../components/Navbar";
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
-      <div className="landingPage"></div>
+      <Home />
+      <div className="services">
+        <h1>Our Services</h1>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut lab
+          </p>
+          <ul>
+            <li>Lorem upsum</li>
+            <li>Lorem upsum</li>
+            <li>Lorem upsum</li>
+            <li>Lorem upsum</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
