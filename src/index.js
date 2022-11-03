ca528657c6b93c30eeb77dc51dc49985bf721a7e
jad amine <jade1992amine@gmail.com>
2022-11-03 21:56:30 +0200

    Changed favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import "./fonts/Montserrat/Montserrat-Medium.ttf";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
