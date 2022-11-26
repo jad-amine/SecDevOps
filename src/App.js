b6d8b8c4ab6ffca37990da400353332dda70acaa
jad amine <jade1992amine@gmail.com>
2022-11-26 16:34:15 +0200

    Adding style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <LandingPage />
+      <Footer />
     </div>
   );
 }
