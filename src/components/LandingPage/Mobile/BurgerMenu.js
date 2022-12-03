0ff47dc2b28e8f9e1e942a30606a9ff1cef35c63
jad amine <jade1992amine@gmail.com>
2022-12-03 14:32:28 +0200

    Created Hamburger menu

---
+++ src/components/LandingPage/Mobile/BurgerMenu.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { IoMdClose } from "react-icons/io";
+
+export default function BurgerMenu({ showBurgerMenu, setShowBurgerMenu }) {
+  return (
+    <div className={showBurgerMenu ? "burger-menu" : "hidden-burger-menu"}>
+      <ul>
+        <IoMdClose
+          onClick={() => setShowBurgerMenu(false)}
+          style={{ alignSelf: "flex-end", marginRight: 10 }}
+          size={25}
+          className="burger-icon-close"
+        />
+        <li style={{ paddingTop: 30 }}>Services</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  );
+}
