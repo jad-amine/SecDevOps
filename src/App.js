719cfd75f62431b2277cd42090e4fbccd0cb03c6
jad amine <jade1992amine@gmail.com>
2022-12-03 14:29:46 +0200

    Added gesture handler swipe functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance + 50;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe)
+      isLeftSwipe ? setShowBurgerMenu(true) : setShowBurgerMenu(false);
+  };
   return (
-    <div className="App">
-      <Navbar />
+    <div
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+      className="App"
+    >
+      <Navbar
+        showBurgerMenu={showBurgerMenu}
+        setShowBurgerMenu={setShowBurgerMenu}
+      />
       <LandingPage />
       <Footer />
     </div>
