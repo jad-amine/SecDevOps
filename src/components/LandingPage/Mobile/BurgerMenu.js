b45755ce8f53bbd92d680186ef8a6d4c5fd9690e
jad amine <jade1992amine@gmail.com>
2022-12-06 08:18:27 +0200

    Fixed Marc website

--- src/components/LandingPage/Mobile/BurgerMenu.js
+++ src/components/LandingPage/Mobile/BurgerMenu.js
@@ -4,13 +4,13 @@ import { IoMdClose } from "react-icons/io";
 export default function BurgerMenu({ showBurgerMenu, setShowBurgerMenu }) {
   return (
     <div className={showBurgerMenu ? "burger-menu" : "hidden-burger-menu"}>
-      <ul>
-        <IoMdClose
+      <ul style={{ paddingTop: "20%" }}>
+        {/* <IoMdClose
           onClick={() => setShowBurgerMenu(false)}
           style={{ alignSelf: "flex-end", marginRight: 10 }}
           size={25}
           className="burger-icon-close"
-        />
+        /> */}
         <li style={{ paddingTop: 30 }}>Services</li>
         <li>About Us</li>
         <li>Contact Us</li>
