7fa08533e0639abc2f02a375a88fe4ef7e753525
jad amine <jade1992amine@gmail.com>
2022-12-21 19:56:33 +0200

    Added About us section

---
+++ src/components/AbouUs/LearnMore.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LearnMore() {
+  const height = window.innerHeight;
+
+  return (
+    <div className="about-us" style={{ height: height }}>
+      <button className="contact-us">Learn More</button>
+    </div>
+  );
+}
