aa1ab2cf2cf0b6631fcb38f432b55f6e1a571a9f
jad amine <jade1992amine@gmail.com>
2023-01-08 10:04:48 +0200

    Added Auto Scroll to top functionality when navigating from page to page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LearnMore from "../components/AbouUs/LearnMore";
 import OurStory from "../components/AbouUs/OurStory";
-import OurTeam from "../components/AbouUs/OurTeam";
+import { useLocation } from "react-router-dom";
+// import OurTeam from "../components/AbouUs/OurTeam";
 // import ContactUs from "../components/AbouUs/ContactUs";
 
 export default function AboutUs() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <>
       <LearnMore />
       <OurStory />
-      <OurTeam />
+      {/* <OurTeam /> */}
       {/* <ContactUs /> */}
     </>
   );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "../components/LandingPage/Home";
 import Services from "../components/LandingPage/Services";
 import Steps from "../components/LandingPage/Steps";
 import Testimonial from "../components/LandingPage/Testimonial";
+import { useLocation } from "react-router-dom";
 
 export default function LandingPage() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <div>
       <Home />
