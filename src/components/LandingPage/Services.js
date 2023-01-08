4bda6405fe848c8ca29b03798ec45fb4e384c0f2
jad amine <jade1992amine@gmail.com>
2023-01-08 10:03:54 +0200

    Changed services order

--- src/components/LandingPage/Services.js
+++ src/components/LandingPage/Services.js
@@ -1,25 +1,25 @@
 import React from "react";
 import Service from "./Service";
-import logo from "../../assets/navbar/logo.png";
+// import logo from "../../assets/navbar/logo.png";
 import RED from "../../assets/LandingPage/ServicesIcons/RED.svg";
 import Breach from "../../assets/LandingPage/ServicesIcons/Breach.svg";
 import PURPLE from "../../assets/LandingPage/ServicesIcons/PURPLE.svg";
 import WebApp from "../../assets/LandingPage/ServicesIcons/WebApp.svg";
-import RANSOMWARE from "../../assets/LandingPage/ServicesIcons/RANSOMWARE.svg";
-import Penetration from "../../assets/LandingPage/ServicesIcons/Penetration.svg";
+// import RANSOMWARE from "../../assets/LandingPage/ServicesIcons/RANSOMWARE.svg";
+// import Penetration from "../../assets/LandingPage/ServicesIcons/Penetration.svg";
 import MobileAccordion from "./Mobile/MobileAccordion";
 
 export default function Services() {
   return (
     <div className="services">
-      <div className="company-info">
+      {/* <div className="company-info">
         <p>
           <span style={{ color: "#F42240" }}>Fight 4 Security</span> is an
           information security company that prioritizes what matters most to
           your organization.
         </p>
         <img src={logo} alt={"Fight 4 Security"} />
-      </div>
+      </div> */}
       <MobileAccordion />
       <div className="our-services">
         <p
@@ -32,34 +32,39 @@ export default function Services() {
         >
           Our Services
         </p>
+
+        {/* ----------- Red Team ---------- */}
+        <div></div>
         <Service
+          color={"red"}
           icon1={Breach}
-          icon2={Penetration}
+          icon2={WebApp}
           title1={"ASSUMED BREACH ASSESSMENT"}
-          title2={"PENETRATION TESTING"}
+          title2={"WEB & MOBILE APPLICATION PENETRATION TESTING"}
           description1={
             "This engagement models an adversary inside of your network to help answer fundamental questions on the impacts post-breach."
           }
           description2={
-            "Our customized service to enumerate systems and vulnerabilities that put your organization at risk."
+            "We perform in-depth analysis to find security issues caused by programming errors, misconfigurations, and application architecture."
           }
         />
         <Service
+          color={"red"}
           icon1={RED}
-          icon2={RANSOMWARE}
+          // icon2={RANSOMWARE}
           title1={"RED TEAM & ADVERSARY EMULATION"}
-          title2={"RANSOMWARE READINESS ASSESSMENT"}
+          // title2={"RANSOMWARE READINESS ASSESSMENT"}
           description1={
             "A goal-oriented approach to test your defensive stack – people, processes, and technologies – against a sophisticated adversary."
           }
-          description2={
-            "We employ a multi-faceted approach to test your organization’s ability to defend against and detect ransomware attacks."
-          }
+          // description2={
+          //   "We employ a multi-faceted approach to test your organization’s ability to defend against and detect ransomware attacks."
+          // }
         />
-        <Service
+        {/* <Service
           icon1={WebApp}
           icon2={PURPLE}
-          title1={"WEB APPLICATION PENETRATION TESTING"}
+          title1={"WEB APPLICATION AND MOBILE PENETRATION TESTING"}
           title2={"PURPLE TEAM"}
           description1={
             "We perform in-depth analysis to find security issues caused by programming errors, misconfigurations, and application architecture."
@@ -67,6 +72,49 @@ export default function Services() {
           description2={
             "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
           }
+        /> */}
+
+        {/* ----------- Blue Team ---------- */}
+        <Service
+          color={"blue"}
+          icon1={PURPLE}
+          icon2={PURPLE}
+          title1={"INCIDENT RESPONSE"}
+          title2={"Advanced Digital Forensics"}
+          description1={
+            "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
+          }
+          description2={
+            "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
+          }
+        />
+        <Service
+          color={"blue"}
+          icon1={PURPLE}
+          // icon2={PURPLE}
+          title1={"SIEM Deployment & Engineering"}
+          // title2={"Advanced Digital Forensics"}
+          description1={
+            "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
+          }
+          // description2={
+          //   "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
+          // }
+        />
+
+        {/* ----------- Purple Team ---------- */}
+        <Service
+          icon1={PURPLE}
+          color={"purple"}
+          // icon2={PURPLE}
+          title1={"PURPLE TEAM"}
+          // title2={"PURPLE TEAM"}
+          description1={
+            "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
+          }
+          // description2={
+          //   "A collaborative engagement with your defenders to hone their ability to identify and respond to advanced attacks"
+          // }
         />
       </div>
     </div>
